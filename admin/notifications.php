f1fc8c71ba8dc6fffe0bd99981810b38d3fa9941
Ben Huson <ben@thewhiteroom.net>
2014-05-13 07:35:14 +0100

    Update names of email notifications.

--- admin/notifications.php
+++ admin/notifications.php
@@ -24,13 +24,13 @@ class Expire_User_Notifications_Admin {
 		$notifications = array(
 			array(
 				'name'         => 'expire_users_notification_message',
-				'notification' => __( 'User Notification Email', 'expire-users' ),
+				'notification' => __( 'User Expired Notification', 'expire-users' ),
 				'description'  => __( 'This email is sent to a user when their login details expire.', 'expire-users' ),
 				'message'      => get_option( 'expire_users_notification_message' )
 			),
 			array(
 				'name'         => 'expire_users_notification_admin_message', 
-				'notification' => __( 'Admin Notification Email', 'expire-users' ),
+				'notification' => __( 'User Expired Admin Notification', 'expire-users' ),
 				'description'  => __( 'This email is sent to the WordPress admin email address when a user expires.', 'expire-users' ),
 				'message'      => get_option( 'expire_users_notification_admin_message' )
 			)
